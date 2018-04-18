eadbc65cd1864873daabda1225aff28e34e9703f
MaksimLavrenyuk95 <maksim.lavrenyuk@gmail.com>
2018-04-18 10:24:23 +0300

    Update app.js

--- FoundationExample/js/app.js
+++ FoundationExample/js/app.js
@@ -1,1 +1,30 @@
+"use strict"
+var s = document.forms.Sum,
+    d = s.querySelectorAll('input[type="checkbox"]:not([value]), input[type="checkbox"][value=""]');
+  for (var i = 0; i < d.length; i++) 
+    d[i].disabled = true;
+  $(document).ready(function() {
+    TweenMax.to( $('#meterClock'), 1, {rotation:81, transformOrigin:"right center"});
+  });
 
+$('input[type="checkbox"]').click(function(){
+      if( $(this).is(':checked') ) {
+	      s.onchange = function() { 
+	      var n = s.querySelectorAll('[type="checkbox"]'),
+			itog = 0;
+		      for(var j=0; j<n.length; j++)
+			n[j].checked ? itog += parseFloat(n[j].value) : itog;
+		      document.getElementById('ClockNumber').innerHTML =  itog + '%';
+		      TweenMax.to( $('#meterClock'), 1, {rotation:itog*1.8, transformOrigin:"right center"});
+			}
+	} else {
+		s.onchange = function() { // начало работы функции сложения
+			var n = s.querySelectorAll('[type="checkbox"]'),
+			  itog = 0;
+			for(var j=0; j<n.length; j++)
+                n[j].checked ? itog += parseFloat(n[j].value) : itog;
+			document.getElementById('ClockNumber').innerHTML =  itog + '%';
+			TweenMax.to( $('#meterClock'), 1, {rotation:itog*1.8, transformOrigin:"right center"});
+			}
+		}
+	});
