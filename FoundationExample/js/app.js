d122bfa6adbaa6a9c1d3e922072c67aa310108a9
MaksimLavrenyuk95 <maksim.lavrenyuk@gmail.com>
2018-04-18 10:23:11 +0300

    Update app.js

--- FoundationExample/js/app.js
+++ FoundationExample/js/app.js
@@ -1,9 +1,1 @@
-"use strict"
-var s = document.forms.Sum,
-    d = s.querySelectorAll('input[type="checkbox"]:not([value]), input[type="checkbox"][value=""]');
-  for (var i = 0; i < d.length; i++) 
-    d[i].disabled = true;
-  $(document).ready(function() {
-    TweenMax.to( $('#meterClock'), 1, {rotation:81, transformOrigin:"right center"});
-  });
 
